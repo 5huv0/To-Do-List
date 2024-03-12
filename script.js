8eadcbcc4a0c0cda2559b7dcd74cc3cdaf0cc079
5huv0 <md88748874@gmail.com>
2024-03-13 01:36:54 +0600

    adding js

---
+++ script.js
@@ -0,0 +1,8 @@
+function addWork(){
+    let a ;
+    const inputs = document.getElementById('input').value;
+    const addToList = document.getElementById('lists').innerHTML;
+    for(a = 0 ; a <= 5 ; a++){
+
+    }
+}
